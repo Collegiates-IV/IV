6593042fcfb471766f80cb67de2cb259f68d744d
MustafaProgram <mustafaakilli24@gmail.com>
2026-03-30 19:17:49 -0400

    Revert "Add YOLO inference placeholder and frontend components"
    This reverts commit 6e9c395b5fa219a34e95a3d579c654402dfa90bf.

--- frontend/SC/lib/supabase/client.ts
+++
@@ -1,4 +0,0 @@
-// This file is deprecated. useSupabase hook should be used instead.
-export const createClient = () => {
-  throw new Error("createClient from utils is deprecated. Use useSupabase() hook from components/supabase-provider.tsx instead.");
-};
